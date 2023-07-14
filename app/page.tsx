891e00198534750cf79cbe4a53183eacdbfc3d3e
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-07-14 16:05:46 +0530

    removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
-import Text from "@/components/text/text";
 import styles from "./page.module.css";
 import Directory from "@/components/directory/directory";
-import Modal from "@/components/modal/modal";
-import { useEffect, useState } from "react";
-import { useScrollLock } from "@/utils/scrollLock";
-import IntegrateOnCanto from "@/sections/integrate";
 
 export default function Home() {
   return (
