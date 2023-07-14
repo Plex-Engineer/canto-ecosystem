3917148ea0a0b75c8141ade9e9f6be1a2c80e542
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-07-14 15:27:40 +0530

    removed scroll lock and modal

--- app/page.tsx
+++ app/page.tsx
@@ -8,38 +8,8 @@ import { useScrollLock } from "@/utils/scrollLock";
 import IntegrateOnCanto from "@/sections/integrate";
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState(false);
-  const { lockScroll, unlockScroll } = useScrollLock();
-
-  useEffect(() => {
-    if (isOpen) {
-      lockScroll(8);
-    } else {
-      unlockScroll();
-    }
-  }, [isOpen, lockScroll, unlockScroll]);
-
   return (
     <main className={styles.main}>
-      <Modal
-        open={isOpen}
-        onClose={() => {
-          setIsOpen(false);
-        }}
-        width="600px"
-        height="500px"
-        title="Integrate on Canto"
-      >
-        <IntegrateOnCanto />
-      </Modal>
-
-      <button
-        onClick={() => {
-          setIsOpen(true);
-        }}
-      >
-        open
-      </button>
       <Directory> directory</Directory>
     </main>
   );
