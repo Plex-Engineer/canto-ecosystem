8a60b87dd5d3e0b83b8cd2f7f39a13ba39b8a00e
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-07-19 17:45:24 +0530

    fixed directory child prop

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Directory from "@/components/directory/directory";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Directory> directory</Directory>
+      <Directory />
     </main>
   );
 }
